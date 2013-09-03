29c5e59b52f73ffd8cbc772870045d3ed7c7a0aa
Nicolas Raoul <nicolas.raoul@gmail.com>
2013-09-03 15:42:59 +0900

    overwrite

--- pull.sh
+++ pull.sh
@@ -1,5 +1,5 @@
 # Pull latest index.html so that demo website uses the latest code
-wget https://raw.github.com/nicolas-raoul/WebCMIS/master/src/index.html
+wget -q https://raw.github.com/nicolas-raoul/WebCMIS/master/src/index.html -O index.html
 git add index.html
 git commit -m "Pull latest index.html"
 git push
